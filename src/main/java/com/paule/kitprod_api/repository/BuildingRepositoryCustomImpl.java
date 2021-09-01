c5171801706c8ddff2080c190c9bfa3d5716feb7
ObonePaule <47106158+ObonePaule@users.noreply.github.com>
2021-09-01 05:38:28 +0200

    Delete and update building features

--- src/main/java/com/paule/kitprod_api/repository/BuildingRepositoryCustomImpl.java
+++ src/main/java/com/paule/kitprod_api/repository/BuildingRepositoryCustomImpl.java
@@ -1,6 +1,7 @@
 package com.paule.kitprod_api.repository;
 
 import com.paule.kitprod_api.model.Building;
+import com.paule.kitprod_api.model.Employee;
 import com.paule.kitprod_api.model.Exploitation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -54,6 +55,47 @@ public class BuildingRepositoryCustomImpl implements IBuildingRepositoryCustom {
         if (!buildingById.isEmpty()) {
 
             return buildingById.get(0);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String idExploitation, String idBuilding) {
+        boolean wasRemoved = false;
+        Optional<Exploitation> exploitation = exploitationRepository.findById(idExploitation);
+
+        if (exploitation.isPresent()) {
+            Exploitation existingExploitation = exploitation.get();
+            wasRemoved = existingExploitation.removeBuilding(idBuilding);
+            exploitationRepository.save(existingExploitation);
+
+            return wasRemoved;
+        }
+
+        return wasRemoved;
+    }
+
+    @Override
+    public Building update(String idExploitation, String idBuilding, Building updatedBuilding) {
+        Optional<Exploitation> exploitation = exploitationRepository.findById(idExploitation);
+
+        if (exploitation.isPresent()) {
+            Exploitation existingExploitation = exploitation.get();
+            List<Building> updatedBuildings = existingExploitation.getBuildings().stream().peek(building -> {
+                if (building.getId().equals(idBuilding)) {
+                    building.setName(updatedBuilding.getName());
+                    building.setSpace(updatedBuilding.getSurface());
+                    building.setNumberOfLots(updatedBuilding.getNumberOfLots());
+                    building.setLots(updatedBuilding.getLots());
+
+                    updatedBuilding.setId(building.getId());
+                }
+            }).collect(Collectors.toList());
+
+            existingExploitation.setBuildings(updatedBuildings);
+            exploitationRepository.save(existingExploitation);
+
+            return updatedBuilding;
         }
         return null;
     }
--- src/main/java/com/paule/kitprod_api/repository/IBuildingRepositoryCustom.java
+++ src/main/java/com/paule/kitprod_api/repository/IBuildingRepositoryCustom.java
@@ -1,10 +1,14 @@
 package com.paule.kitprod_api.repository;
 
 import com.paule.kitprod_api.model.Building;
+import com.paule.kitprod_api.model.Employee;
+
 import java.util.List;
 
 public interface IBuildingRepositoryCustom {
     public Building insert(String idExploitation, Building building);
     public List<Building> findAll(String idExploitation);
     public Building findById(String idExploitation, String idBuilding);
+    public boolean delete(String idExploitation, String idBuilding);
+    public Building update(String idExploitation, String idBuilding, Building building);
 }
